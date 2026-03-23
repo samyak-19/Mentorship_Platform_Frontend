079a4ccf2dff81ac2cf49c99a8cb44945a3a8e61
samyak-19 <samlinkin19@gmail.com>
2026-03-24 01:56:54 +0530

    Add authentication with Supabase (signup, login, role setup)

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://sdljeynmrmumvqkjxzjw.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkbGpleW5tcm11bXZxa2p4emp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyODI3MzAsImV4cCI6MjA4OTg1ODczMH0.IE7M4yDKvfhQEwWaC6ljAIlM3loFR9l-2ii3J7niWSU";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey =  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
